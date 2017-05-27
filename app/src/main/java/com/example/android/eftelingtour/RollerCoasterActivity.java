621c9bf69e02e10c9385185204ad37076e83ef58
Jeffrey Leijten <jeffreyleijten@live.nl>
2017-05-27 16:08:48 +0200

    Create RollerCoaster activity add mock files

---
+++ app/src/main/java/com/example/android/eftelingtour/RollerCoasterActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.eftelingtour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class RollerCoasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attraction_list);
+
+        final ArrayList<Attraction> attractions = new ArrayList<>(0);
+        attractions.add(new Attraction("Baron 1898", R.drawable.baron_1898));
+        attractions.add(new Attraction("Python", R.drawable.baron_1898));
+    }
+}
