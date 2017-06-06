4a88d18d83116e70f58f798859dadd746a11b2da
Jeffrey Leijten <jeffreyleijten@live.nl>
2017-06-06 13:29:30 +0200

    Expand Coaster List

--- app/src/main/java/com/example/android/eftelingtour/RollerCoasterFragment.java
+++ app/src/main/java/com/example/android/eftelingtour/RollerCoasterFragment.java
@@ -20,10 +20,18 @@ public class RollerCoasterFragment extends android.support.v4.app.Fragment{
         View rootView = inflater.inflate(R.layout.attraction_list, container, false);
 
         final ArrayList<Attraction> attractions = new ArrayList<>(0);
-        attractions.add(new Attraction("Baron 1898", "Ruigrijk", 140, -1,
-                "Exciting dive coaster with a free fall of 37.5 meters", R.drawable.baron_1898));
-        attractions.add(new Attraction("Python", "Ruigrijk", 120, -1,
-                "Survive the twists and turns at great heights on this steel roller coaster with its double loop and corkscrew.",R.drawable.baron_1898));
+        attractions.add(new Attraction(getString(R.string.baron_1898), getString(R.string.ruigrijk),
+                140, -1, getString(R.string.baron_1898_description), R.drawable.baron_1898));
+        attractions.add(new Attraction(getString(R.string.bob), getString(R.string.anderrijk),
+                -1, 120, getString(R.string.bob_description), R.drawable.bob));
+        attractions.add(new Attraction(getString(R.string.python), getString(R.string.ruigrijk),
+                120, -1, getString(R.string.python_description), R.drawable.python));
+        attractions.add(new Attraction(getString(R.string.vogel_rok), getString(R.string.reizenrijk),
+                120, -1, getString(R.string.vogel_rok_description), R.drawable.vogel_rok));
+        attractions.add(new Attraction(getString(R.string.de_vliegende_hollander), getString(R.string.ruigrijk),
+                120, -1, getString(R.string.de_vliegende_hollander_description), R.drawable.de_vliegende_hollander));
+        attractions.add(new Attraction(getString(R.string.joris_en_de_draak), getString(R.string.ruigrijk),
+                110, 120, getString(R.string.joris_en_de_draak_description), R.drawable.joris_en_de_draak));
 
         AttractionAdapter adapter = new AttractionAdapter(getActivity(), attractions);
 
